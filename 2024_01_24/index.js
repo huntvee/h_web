68e737760910f15f01e4247340635ff2cca5e73e
huntvee <telotest@gmail.com>
2024-01-24 12:39:36 +0100

    tguzu

---
+++ 2024_01_24/index.js
@@ -0,0 +1,14 @@
+function osszegzes(l){
+    let ossz=0
+    for (let i = 0; i < l.length; i++){
+        ossz+l[i] 
+    }
+    return(ossz)
+}
+function megszamlakas(l){
+    let c = 0
+    for (let i = 0; i< l.length; i++){
+        c++
+    }
+    return(c)
+}
